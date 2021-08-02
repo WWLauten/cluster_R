e762bd00adc8ffb4aaa9e7fd6cdb34e0165c447c
Willian W. Lautenschlager <willianw@uol.com.br>
2021-08-02 19:28:49 -0300

    l-2-a-4-par

--- cluster_r.R
+++ cluster_r.R
@@ -12,3 +12,14 @@ View(filmes)
 ## lendo os dados pre-processados
 filmes_transf <- read.csv2('movies_transf.csv')
 View(filmes_transf)
+
+## Para usar o scale todas as colunas devem ser numéricas.
+## Vamos usar o dplyr para isso o %>% significa concatenação de atributos (param)
+## o sinal de menos em frente aos nomes das colunas significam que queremos selecionar tudo exceto 
+## aquela colunas.
+filmes_transf <- filmes_transf %>%
+                  select(-movieId, -titulo)
+
+
+## normalizar as bases de dados utilizando scale.
+dados_normalizados <- scale(filmes_transf)
